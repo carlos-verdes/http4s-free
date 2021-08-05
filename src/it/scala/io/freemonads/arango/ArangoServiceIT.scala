d98fb1afa58f93ba7f1895a0f82642899dbaf2a2
Carlos Verdes <cverdes@gmail.com>
2021-08-05 12:50:10 +0400

    dsl for RestResource http responses

--- src/it/scala/io/freemonads/arango/ArangoServiceIT.scala
+++ src/it/scala/io/freemonads/arango/ArangoServiceIT.scala
@@ -8,15 +8,22 @@ package arango
 import scala.concurrent.Future
 
 import avokka.velocypack.{VPackDecoder, VPackEncoder}
-import cats.effect.IO
-import cats.~>
+import cats.data.EitherK
+import cats.effect.{IO, Sync, Timer}
+import cats.{Functor, ~>}
 import com.whisk.docker.impl.spotify._
 import com.whisk.docker.specs2.DockerTestKit
+import io.circe.generic.auto._
 import io.freemonads.arango.docker.DockerArango
 import io.freemonads.http.resource.{ResourceAlgebra, RestResource}
+import io.freemonads.http.rest
 import io.freemonads.specs2.Http4FreeIOMatchers
 import org.http4s._
-import org.http4s.implicits.http4sLiteralsSyntax
+import org.http4s.dsl.Http4sDsl
+import org.http4s.circe.CirceEntityCodec._
+import org.http4s.dsl.io.Created
+import org.http4s.headers.Location
+import org.http4s.implicits.{http4sKleisliResponseSyntaxOptionT, http4sLiteralsSyntax}
 import org.specs2.Specification
 import org.specs2.matcher.{IOMatchers, MatchResult}
 import org.specs2.specification.core.{Env, SpecStructure}
@@ -28,13 +35,18 @@ trait MockServiceWithArango extends IOMatchers {
 
   import api._
   import http.resource._
+  import rest._
 
   implicit def unsafeLogger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   case class Mock(id: String, user: String, age: Int)
+  case class UserRequest(address: String)
+  case class User(address: String, nonce: String)
 
   implicit val mockEncoder: VPackEncoder[Mock] = VPackEncoder.gen
   implicit val mockDecoder: VPackDecoder[Mock] = VPackDecoder.gen
+  implicit val userEncoder: VPackEncoder[User] = VPackEncoder.gen
+  implicit val userDecoder: VPackDecoder[User] = VPackDecoder.gen
 
   implicit val arangoInterpreter: ResourceAlgebra ~> IO = arangoIoInterpreter
 
@@ -69,6 +81,42 @@ trait MockServiceWithArango extends IOMatchers {
 
   def fetchFromArango(uri: Uri)(implicit dsl: ArangoDsl[ResourceAlgebra]): ApiFree[ResourceAlgebra, Mock] =
     dsl.fetch[Mock](uri).map(_.body)
+
+
+  val userAddress = "address1"
+  val userNonce = "nonce1"
+  val userRequest = UserRequest(userAddress)
+  val expectedUser = User(userAddress, userNonce)
+
+  def userUri(user: User): Uri = uri"/users" / user.address
+
+  def userRoutes[F[_]: Sync : Timer : Functor, Algebra[_]](
+      implicit http4sFreeDsl: Http4sFreeDsl[Algebra],
+      resourceDsl: ResourceDsl[Algebra, VPackEncoder, VPackDecoder],
+      interpreters: Algebra ~> F): HttpRoutes[F] = {
+
+    val dsl = new Http4sDsl[F]{}
+    import dsl._
+    import http4sFreeDsl._
+    import resourceDsl._
+
+    HttpRoutes.of[F] {
+      case r @ POST -> Root / "users" =>
+        for {
+          userRequest <- parseRequest[F, UserRequest](r)
+          userAddress = userRequest.address
+          user = User(userAddress, userNonce)
+          storedUser <- store[User](userUri(user), user)
+        } yield storedUser.created[F]
+    }
+  }
+
+  implicit val interpreters = (http4sInterpreter[IO] or arangoIoInterpreter)
+  type CombinedAlgebra[R] = EitherK[Http4sAlgebra, ResourceAlgebra, R]
+
+  val userService = userRoutes[IO, CombinedAlgebra]
+
+  val createUserRequest: Request[IO] = Request[IO](Method.POST, uri"/users").withEntity(userRequest)
 }
 
 class ArangoServiceIT(env: Env)
@@ -88,6 +136,8 @@ class ArangoServiceIT(env: Env)
       Store and fetch from ArangoDB                           $storeAndFetch
       Store and update from ArangoDB                          $storeAndUpdate
       Return not found error when document doesn't exist      $returnNotFound
+      Store a resource usind HTTP APi                         $storeResource
+
   """
 
   def arangoIsReady: MatchResult[Future[Boolean]] = isContainerReady(arangoContainer) must beTrue.await
@@ -99,4 +149,12 @@ class ArangoServiceIT(env: Env)
   def storeAndUpdate: MatchResult[Any] = storeAndUpdate(mocksUri, mock1, updatedMock) must resultOk(updatedMock)
 
   def returnNotFound: MatchResult[Any] = fetchFromArango(uri"/emptyCollection/123") must resultErrorNotFound
+
+  def storeResource: MatchResult[Any] =
+    userService.orNotFound(createUserRequest) must returnValue { (response: Response[IO]) =>
+      response must haveStatus(Created) and
+          (response must haveBody(expectedUser)) and
+          (response must containHeader(Location(userUri(expectedUser))))
+    }
+
 }
--- src/it/scala/io/freemonads/http/Http4sFreeIT.scala
+++ src/it/scala/io/freemonads/http/Http4sFreeIT.scala
@@ -6,16 +6,12 @@
 package io.freemonads
 package http
 
-import java.util.UUID
 
-import avokka.velocypack.{VPackDecoder, VPackEncoder}
-import cats.data.EitherK
 import cats.effect.{IO, Sync, Timer}
 import cats.{Functor, ~>}
 import io.circe.generic.auto._
 import io.circe.literal.JsonStringContext
-import io.freemonads.http.resource.{ResourceAlgebra, ResourceDsl, RestResource}
-import io.freemonads.specs2.Http4FreeMatchers
+import io.freemonads.specs2.Http4FreeIOMatchers
 import org.http4s._
 import org.http4s.circe.CirceEntityCodec._
 import org.http4s.dsl.Http4sDsl
@@ -30,34 +26,27 @@ import org.typelevel.log4cats.slf4j.Slf4jLogger
 trait RestRoutes extends IOMatchers {
 
   import api._
-  import arango._
   import rest._
 
   case class Mock(id: Option[String], name: String, age: Int)
-  case class UserRequest(userId: String)
-  case class User(publicAddress: String, nonce: String)
-
-  implicit val mockEncoder: VPackEncoder[Mock] = VPackEncoder.gen
-  implicit val mockDecoder: VPackDecoder[Mock] = VPackDecoder.gen
-  implicit val userEncoder: VPackEncoder[User] = VPackEncoder.gen
-  implicit val userDecoder: VPackDecoder[User] = VPackDecoder.gen
 
   val newMock = Mock(None, "name123", 23)
   val createdId = "id123"
   val createdMock = Mock(Some(createdId), "name123", 23)
+
+  val userAddress = "address1"
+  val userNonce = "nonce1"
 
   val existingId = "id456"
   val existingMock = Mock(Some(existingId), "name123", 23)
 
   def mockRoutes[F[_]: Sync : Timer : Functor, Algebra[_]](
       implicit http4sFreeDsl: Http4sFreeDsl[Algebra],
-      resourceDsl: ResourceDsl[Algebra, VPackEncoder, VPackDecoder],
       interpreters: Algebra ~> F): HttpRoutes[F] = {
 
     val dsl = new Http4sDsl[F]{}
     import dsl._
     import http4sFreeDsl._
-    import resourceDsl._
 
     HttpRoutes.of[F] {
       case GET -> Root / "mocks" =>
@@ -69,34 +58,24 @@ trait RestRoutes extends IOMatchers {
         for {
           mockRequest <- parseRequest[F, Mock](r)
         } yield Created(mockRequest.copy(id = Some(createdId)), Location(uri"/mocks" / createdId))
-
-      case r @ POST -> Root / "users" =>
-        for {
-          userRequest <- parseRequest[F, UserRequest](r)
-          userId = userRequest.userId
-          nonce = UUID.randomUUID().toString
-          RestResource(userUri, storedUser) <- store[User](uri"/users" / userId, User(userId, nonce))
-        } yield Created(storedUser, Location(userUri))
     }
   }
 
   implicit def unsafeLogger: Logger[IO] = Slf4jLogger.getLogger[IO]
-  implicit val interpreters = (http4sInterpreter[IO] or arangoIoInterpreter)
+  implicit val interpreters = http4sInterpreter[IO]
 
-  type CombinedAlgebra[R] = EitherK[Http4sAlgebra, ResourceAlgebra, R]
-
-  val mockService = mockRoutes[IO, CombinedAlgebra]
+  val mockService = mockRoutes[IO, Http4sAlgebra]
 
   val createMockRequest: Request[IO] = Request[IO](Method.POST, uri"/mocks").withEntity(newMock)
   val invalidRequest: Request[IO] = Request[IO](Method.POST, uri"/mocks").withEntity(json"""{ "wrongJson": "" }""")
   val notFoundRequest: Request[IO] = Request[IO](Method.GET, uri"/wrongUri")
 }
 
-class Http4sFreeIT extends Specification with RestRoutes with Http4FreeMatchers[IO] with IOMatchers {
+class Http4sFreeIT extends Specification with RestRoutes with Http4FreeIOMatchers {
 
   def is: SpecStructure =
       s2"""
-          RestSpec should: <br/>
+          Http4s general IT test should: <br/>
           Be able to use free monads on Rest APIs        $httpWithFreeMonads
           Response with 400 error if request parse fails $manageParsingErrors
           Response with 403 error on auth issues         $manageAuthErrors
